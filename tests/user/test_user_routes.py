69bac7097636124a7cb8fcd3a94b28496ac42ada
Raaxxy <rahularyan0197@gmail.com>
2024-01-30 18:53:42 +0530

    added tests for login

--- tests/user/test_user_routes.py
+++ tests/user/test_user_routes.py
@@ -5,6 +5,15 @@ from faker import Faker
 fake = Faker()
 client = TestClient(app)
 
+def get_user_data():
+    user_data = {
+        "username" : fake.user_name(),
+        "email" : fake.email(),
+        "password" : "password1"
+    }
+
+    response = client.post('/user/signup/', json=user_data)
+    return user_data
 
 def test_signup():
     signup_data = {
@@ -19,3 +28,11 @@ def test_signup():
     assert response.json() == {
         "message": "User has been created successfully"
     }
+
+def test_login():
+    login_data = get_user_data()
+    response = client.post('/user/login/', json=login_data)
+
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    assert response.json()['token_type'] == 'bearer'
