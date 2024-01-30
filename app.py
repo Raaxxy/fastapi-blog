44d590942cc98d30111254aca29e34f30ef73fc9
Raaxxy <rahularyan0197@gmail.com>
2024-01-30 19:32:11 +0530

    add post router

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 from database import Base,engine
 from core.user.routes import user_router
+from core.post.routes import post_router
 
 app = FastAPI()
 app.include_router(user_router,prefix='/user')
+app.include_router(post_router)
 Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
