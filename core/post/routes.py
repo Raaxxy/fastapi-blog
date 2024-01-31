f7bddbe99c5e965f5bc01128478d17d0c7cb271e
Raaxxy <rahularyan0197@gmail.com>
2024-01-31 11:25:59 +0530

    add list post and view post endpoints

--- core/post/routes.py
+++ core/post/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends , HTTPException
 from sqlalchemy.orm import Session
 from dependencies import get_db,get_current_user
 from core.user.models import User
@@ -16,5 +16,18 @@ def create_post(post_data:CreateUpdatePost,db: Session = Depends(get_db),user:Us
         "data" : post_data
     }
 
-
+@post_router.get('/posts')
+def list_post(db:Session = Depends(get_db),user : User=Depends(get_current_user)):
+    posts = db.query(Post).all()
+    return {
+        "data" : [post.__dict__ for post in posts]
+    }
+@post_router.get("/post/{post_id}")
+def view_post(post_id:str , db:Session= Depends(get_db),user: User=Depends(get_current_user)):
+    post = db.query(Post).filter(Post.id==post_id).first()
+    if not post:
+        raise HTTPException(status_code=404,detail="Post not found")
+    return{
+        "data" : post.__dict__
+    }
 
